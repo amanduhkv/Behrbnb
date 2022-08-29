009bdd022e7438112fa4f4878d4b8feee86eb678
Amanda Vien <amandakvien@gmail.com>
2022-08-29 16:07:53 -0700

    Local Post route working for Spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,5 +9,21 @@ router.get('/', async(req, res) => {
   return res.json(spots);
 });
 
+router.post('/', async(req,res) => {
+  const { address, city, state, country, lat, lng, name, description, price } = req.body;
+  const newSpot = await Spot.create({
+    address,
+    city,
+    state,
+    country,
+    lat,
+    lng,
+    name,
+    description,
+    price
+  });
+  return res.json(newSpot);
+})
+
 
 module.exports = router;
