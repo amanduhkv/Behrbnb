b7a127c347ebb4c53e87b7e3f7cd6bb460b0b4c9
Amanda Vien <amandakvien@gmail.com>
2022-09-16 14:07:23 -0700

    Restore session user thunk action created

--- frontend/behrbnb/src/store/session.js
+++ frontend/behrbnb/src/store/session.js
@@ -34,6 +34,13 @@ export const login = (user) => async (dispatch) => {
   // }
 };
 
+export const restoreUser = () => async dispatch => {
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
