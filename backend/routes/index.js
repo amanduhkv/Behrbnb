651f2b9dbc0122fdb60982f03fd78fdf6a8da8b3
Amanda Vien <amandakvien@gmail.com>
2022-09-16 13:02:47 -0700

    Wrapping fetch requests with CSRF

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -19,4 +19,35 @@ router.get("/api/csrf/restore", (req, res) => {
 
 router.use('/api', apiRouter);
 
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+
+  // Add a XSRF-TOKEN cookie in development
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
 module.exports = router;
--- frontend/behrbnb/src/index.js
+++ frontend/behrbnb/src/index.js
@@ -4,11 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 import configureStore from './store';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
