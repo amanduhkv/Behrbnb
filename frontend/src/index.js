f0c1bea36207d2b99ccb7ac3b289e00375baed54
Amanda Vien <amandakvien@gmail.com>
2022-09-17 19:46:10 -0700

    merge conflict resolution

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
-import './index.css';
-import App from './App';
-import configureStore from './store';
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -18,31 +9,19 @@ import { ModalProvider } from "./context/Modal";
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
->>>>>>> dev
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
-<<<<<<< HEAD
-  window.store = store;
-=======
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
->>>>>>> dev
 }
 
 function Root() {
   return (
-<<<<<<< HEAD
-    <ReduxProvider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ReduxProvider>
-=======
     <Provider store={store}>
       <ModalProvider>
         <BrowserRouter>
@@ -50,7 +29,6 @@ function Root() {
         </BrowserRouter>
       </ModalProvider>
     </Provider>
->>>>>>> dev
   );
 }
 
