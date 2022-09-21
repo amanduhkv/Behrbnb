fa014f429d9c2e216a0ece1c6d2b2294fcf787dc
Amanda Vien <amandakvien@gmail.com>
2022-09-21 00:42:24 -0700

    added css to review page

--- frontend/src/components/Reviews/Reviews.js
+++ frontend/src/components/Reviews/Reviews.js
@@ -2,7 +2,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom";
 import { getReviews } from "../../store/reviews";
-
+import './Reviews.css'
+import bear from '../../assets/bear.svg';
 
 const Reviews = () => {
   const { spotId } = useParams();
@@ -16,17 +17,28 @@ const Reviews = () => {
     dispatch(getReviews(spotId))
   }, [dispatch, spotId]);
 
-  if(!reviews) return null;
+  if (!reviews) return null;
 
   return (
-    <div>
-      hello
-      {reviewsArr.map(review => (
-        <div key={review.id}>
-          <div>{review.review}</div>
-          <div>{review.stars}</div>
+    <div id='review-container'>
+
+        <div id='review-title'>
+          <i className="fa-sharp fa-solid fa-star"></i>
+
+          <h2>{reviewsArr.length} reviews</h2>
         </div>
-      ))}
+        {reviewsArr.map(review => (
+          <div key={review.id}>
+            {/* <div>{review.stars}</div> */}
+            <div id='bear-name'>
+              <img id='bear' src={bear} />
+              <div id='reviewer-name'>{review.User?.firstName}</div>
+            </div>
+            <div id='reviewer-review'>{review.review}</div>
+
+          </div>
+        ))}
+      
     </div>
   )
 }
