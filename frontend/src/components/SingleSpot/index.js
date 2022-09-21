d95584dc4efd7ab5862eed701a9a32b1155d6098
Amanda Vien <amandakvien@gmail.com>
2022-09-20 22:41:43 -0700

    console does not hate me anymore, initial review seeder data broke stuff

--- frontend/src/components/SingleSpot/index.js
+++ frontend/src/components/SingleSpot/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, Route, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { getASpot } from '../../store/spots';
 import { Modal } from '../../context/Modal';
 import EditSpotForm from '../Spots/EditSpotForm';
@@ -10,13 +10,13 @@ const SingleSpot = () => {
   const { spotId } = useParams();
   const sessionUser = useSelector(state => state.session.user)
   const singleSpot = useSelector(state => state.spots.singleSpot);
-  // console.log('single spot', singleSpot)
+  console.log('single spot', singleSpot)
   const dispatch = useDispatch();
   const [editModal, setEditModal] = useState(false);
 
   useEffect(() => {
     dispatch(getASpot(spotId));
-  }, [dispatch])
+  }, [dispatch, spotId])
 
   if (!singleSpot) return null;
 
@@ -42,14 +42,15 @@ const SingleSpot = () => {
         <span>
           <i class="fa-sharp fa-solid fa-star"></i>
         </span>
-        <span>{singleSpot.avgStarRating}</span>
+        {/* dis wrong v */}
+        {/* <span>{singleSpot.avgRating}</span> */}
         <span>{singleSpot.numReviews} reviews</span>
         <span>
           {`${singleSpot.city}, ${singleSpot.state}, ${singleSpot.country}`}
         </span>
       </div>
       <div>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn2rxCQ-CBZqmyMh9k5H5T-LgcBlN7TbK-Sw&usqp=CAU' />
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn2rxCQ-CBZqmyMh9k5H5T-LgcBlN7TbK-Sw&usqp=CAU' alt='polar-bears' />
       </div>
       {/* <p>{`Entire home hosted by ${singleSpot.Owner.firstName}`}</p> */}
       <hr></hr>
---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,7 @@
+const GET_REVIEWS = 'reviews/GET_REVIEWS';
+
+const loadReviews = (reviews) => ({
+  type: GET_REVIEWS,
+  reviews
+});
+
