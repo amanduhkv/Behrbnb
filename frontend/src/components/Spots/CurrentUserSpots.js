8cdafbc48233bb8ff31c596a4d595da19aa4523f
Amanda Vien <amandakvien@gmail.com>
2022-09-25 01:08:21 -0700

    css for current user spots

--- frontend/src/components/Spots/CurrentUserSpots.js
+++ frontend/src/components/Spots/CurrentUserSpots.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from 'react-router-dom';
 import { getSpotCurrentUser } from '../../store/spots';
+import './CurrentUserSpots.css'
 
 const GetSpotsCurrentUser = () => {
   const spots = useSelector(state => {
@@ -20,19 +21,28 @@ const GetSpotsCurrentUser = () => {
   return (
     <div className='card'>
       {spotsArr.map(spot => (
-        <div key={spot.id}>
-          <div className='each-card'>
-            <NavLink to={`/spots/${spot.id}`}>
-              <img className='spot-image' src='https://t4.ftcdn.net/jpg/02/65/70/73/360_F_265707361_N79UdgpbERwsjHZxIWRixuodCKqYsBF5.jpg' />
-            </NavLink>
-            <div className='city-state'>{`${spot.city}, ${spot.state}`}</div>
-            <div className='price'>
-              <span id='number'>{`$${spot.price}`}</span>
-              <span id='night'>night</span>
+        <div id='card-info' key={spot.id}>
+          <NavLink to={`/spots/${spot.id}`}>
+            <img className='spot-image' src={spot.previewImage} />
+          </NavLink>
+          <div id='each-one'>
+            <div className='each-card'>
+              <div className='city-state'>{`${spot.city}, ${spot.state}`}</div>
+              <div id='extra-info'>
+                <div>{spot.country}</div>
+                <div>{spot.name}</div>
+              </div>
+              <div className='price'>
+                <span id='number'>{`$${spot.price}`}</span>
+                <span id='night'>night</span>
+              </div>
             </div>
-            <div className='reviews'>
-              <i className="fa-sharp fa-solid fa-star"></i>
-              {`${spot.avgRating}`}
+
+            <div className='reviews-1'>
+              <i id='star-spot' className="fa-sharp fa-solid fa-star"></i>
+              <div>
+                {spot.avgRating}
+              </div>
             </div>
           </div>
         </div>
